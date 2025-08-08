bf450a87e46cfa3a7055508c733f11e79de98cda
enowbrenda <enowbrenda91106@gmail.com>
2025-08-08 13:42:43 +0100

    trying to allow https request

--- src/main/java/com/brenda/clexis/clientGatewayService/Config/SwaggerConfig.java
+++ src/main/java/com/brenda/clexis/clientGatewayService/Config/SwaggerConfig.java
@@ -5,18 +5,22 @@ import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.security.SecurityRequirement;
 import io.swagger.v3.oas.models.security.SecurityScheme;
+import io.swagger.v3.oas.models.servers.Server;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+import java.util.List;
 
 @Configuration
 public class SwaggerConfig {
     @Bean
     public OpenAPI customOpenAPI() {
         return new OpenAPI()
+                .servers(List.of(new Server().url("https://clexis-gateway.up.railway.app")))
                 .components(new Components()
                         .addSecuritySchemes("bearer-jwt",
                                 new SecurityScheme().type(SecurityScheme.Type.HTTP).scheme("bearer").bearerFormat("JWT")))
                 .addSecurityItem(new SecurityRequirement().addList("bearer-jwt"))
-                .info(new Info().title("The Client Gateway Service").version("v1").description("This is a client gateway service for the momokash loan as a service system"));
+                .info(new Info().title("The Client Gateway Service").version("v1").description("This is a client gateway service for clexis a learning platform"));
     }
 }
